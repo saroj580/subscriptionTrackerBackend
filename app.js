4f496e5447d4088e7fa7c9ac92b3a592ce7c78ba
saroj580 <chaudharysaroj819@gmail.com>
2025-07-08 17:34:51 +0530

    implemented arcjet to save from the spamming(rate limiting)

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(arcjetMiddleware)
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
--- config/env.js
+++ config/env.js
@@ -4,4 +4,4 @@ config( {
     path: `.env.${process.env.NODE_ENV || 'development'}.local`,
 })
 
-export const {PORT, NODE_ENV, DB_URI, JWT_EXPIRES_IN, JWT_SECRET } = process.env;
+export const {PORT, NODE_ENV, DB_URI, JWT_EXPIRES_IN, JWT_SECRET, ARCJET_ENV, ARCJECT_KEY } = process.env;
